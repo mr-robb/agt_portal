581b784bacd707876710fccf3dbb2f8c258a384c
mr_robb <robert101fm@hotmail.com>
2016-06-09 18:23:12 -0500

    Se agrega campo STATUS en el Mapper para poder validar dicho campo en el LoginService

--- src/main/java/mx/com/ebs/inter/controller/LogoutController.java
+++ src/main/java/mx/com/ebs/inter/controller/LogoutController.java
@@ -1,5 +1,11 @@
 package mx.com.ebs.inter.controller;
 
+import mx.com.ebs.inter.data.bo.UserDataBo;
+import mx.com.ebs.inter.data.dao.agt.RecSessionUserMapper;
+import mx.com.ebs.inter.data.model.RecSessionUser;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Timestamp;
 
 /**
  * Created by robb on 29/04/2015.
@@ -17,8 +24,19 @@ public class LogoutController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         if( session != null ) {
+            WebApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+
+            UserDataBo dataBo = (UserDataBo) session.getAttribute("userData");
+            if( dataBo != null ) {
+                RecSessionUser sessionUser = new RecSessionUser();
+                sessionUser.setEBS_USER_ID(dataBo.getEbsUserId());
+                sessionUser.setDESTROYED_TS(new Timestamp(System.currentTimeMillis()));
+                RecSessionUserMapper userMapper = appContext.getBean(RecSessionUserMapper.class);
+                userMapper.updateLogout(sessionUser);
+            }
             session.removeAttribute("userData");
             session.invalidate();
+
         }
         RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
         dispatcher.forward(request,response);
